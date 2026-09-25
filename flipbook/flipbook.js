4a84a710c1edb45ae14e0b2fa6fa85b540b082f0
Pratik Adhikari <amababa055@gmail.com>
2026-09-25 11:59:33 +0545

    Add files via upload

---
+++ flipbook/flipbook.js
@@ -0,0 +1,32 @@
+/******************************
+* RESPONSIVE WARNING BEHAVIOR *
+******************************/
+
+const responsiveWarning = document.getElementById("responsive-warning");
+// Enable/disable responsive warning.
+const responsiveDesign = true;
+// Mobile width limit.
+const threshold = 768;
+
+// Show or hide modal based on screen size.
+function checkResponsiveState() {
+  const small = window.innerWidth <= threshold;
+
+  if (!responsiveDesign && small) {
+    if (!responsiveWarning.open) {
+      responsiveWarning.showModal();
+      document.body.classList.add("overflow-hidden");
+    }
+  } else {
+    if (responsiveWarning.open) {
+      responsiveWarning.close();
+      document.body.classList.remove("overflow-hidden");
+    }
+  }
+}
+
+// Initial check.
+checkResponsiveState();
+
+// Real-time resize detection.
+window.addEventListener("resize", checkResponsiveState);
